0881d89f1812e6a9fe963f49c245fdfa7ba619dc
Meliodas <erencorn@gmail.com>
2018-09-29 02:35:57 +0200

    idk what kind of crack i was on to do this dumb shit

--- events/loginEvent.py
+++ events/loginEvent.py
@@ -141,15 +141,14 @@ def handle(tornadoRequest):
 		# Send login notification before maintenance message
 		if responseToken.admin:
 			responseToken.enqueue(serverPackets.notification("Thank you for being an admin on Mansions, " + username))
-		else if responseToken.owner:
-			responseToken.enqueue(serverPackets.notification("All Praise our lord and savior, " + username))
+
 		else:
 			if glob.conf.extra["type"] == "regular":
 				responseToken.enqueue(serverPackets.notification("Welcome to Mansions, " + username))
 			elif glob.conf.extra["type"] == "relax":
 				responseToken.enqueue(serverPackets.notification("Welcome to Mansion's Relax Server, " + username))
-			elif glob.conf.extra["type"] == "alpha":
-				responseToken.enqueue(serverPackets.notification("Welcome to Mansions!Alpha, " + username))
+			elif responseToken.owner:
+				responseToken.enqueue(serverPackets.notification("All Praise our lord and savior, " + username))
 			else:
 				responseToken.enqueue(serverPackets.notification("Welcome to Mansions, " + username))
 				
